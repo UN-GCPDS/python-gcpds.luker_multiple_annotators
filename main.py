3a8d27ccc3ce81555f2e12c0e21389a64665cd65
jclugor <jclugor@unal.edu.co>
2024-08-26 07:17:12 -0500

    api update

--- main.py
+++ main.py
@@ -1,30 +1,48 @@
-from fastapi import FastAPI, HTTPException
-import pickle
-import os
-from pydantic import BaseModel
+from fastapi import FastAPI, Query
+import tensorflow as tf
+from src import models
 
 app = FastAPI()
 
+fq_vars = ['humedad - determinador halogeno', 'grasa - nmr', 'granulometría - micrometro',
+           'viscosidad plastica  anton paar', 'limite de fluidez  anton paar']
+
+sens_vars = ['acidez', 'amargor', 'aroma', 'astringencia', 'dulce', 'dureza',
+             'impresion global', 'velocidad fusion']
+
+def load_model_sens():
+    models_sens = []
+    for var in sens_vars:
+        models_sens.append(tf.keras.models.load_model(f"models/gcce/gcce_ma_{var}.keras"))
+    return models.MultiOutputModel(models)
+
+model_to_sens = load_model_sens()
+model_to_fq = tf.keras.models.load_model('models/gcce/model_s2fq.keras')
 # Load the models from the repository
-models = {}
-model_path = "models"  # Adjust the path if necessary
-for model_file in os.listdir(model_path):
-    if model_file.endswith('.pkl'):
-        model_name = model_file.split('.')[0]
-        with open(os.path.join(model_path, model_file), 'rb') as f:
-            models[model_name] = pickle.load(f)
 
-# Define the request body
-class PredictionRequest(BaseModel):
-    model_name: str
-    data: list
+@app.get("/predict_sens")
+async def predict_sens(
+    humedad: float = Query(...),
+    grasa: float = Query(...),
+    granulometria: float = Query(...),
+    viscosidad: float = Query(...),
+    fluidez: float = Query(...)
+):
+    input_data = [[humedad, grasa,granulometria, viscosidad, fluidez]]
+    predictions = model_to_sens.predict(input_data)
+    return {"sens": predictions}
 
-# Define the predict endpoint
-@app.post("/predict/")
-async def predict(request: PredictionRequest):
-    model = models.get(request.model_name)
-    if model:
-        prediction = model.predict([request.data])
-        return {"prediction": prediction.tolist()}
-    else:
-        raise HTTPException(status_code=404, detail="Model not found")
+@app.get("/predict_fq")
+async def predict_fq(
+    acidez: float = Query(...),
+    amargor: float = Query(...),
+    aroma: float = Query(...),
+    astringencia: float = Query(...),
+    dulce: float = Query(...),
+    dureza: float = Query(...),
+    impresion: float = Query(...),
+    fusion: float = Query(...)
+):
+    input_data = [[acidez, amargor, aroma, astringencia, dulce, dureza, impresion, fusion]]
+    predictions = model_to_fq.predict(input_data)
+    return {"fq": predictions}
