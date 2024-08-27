58a3a92bb87461b581e3ee5f37f9c0d7dd035a28
jclugor <jclugor@unal.edu.co>
2024-08-27 17:44:39 -0500

    api ready

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI, Query
 import tensorflow as tf
 from src import models
-from multiple_annotators_models import MA_GCCE
+from src.multiple_annotators_models import MA_GCCE
+import numpy as np
 app = FastAPI()
 
 fq_vars = ['humedad - determinador halogeno', 'grasa - nmr', 'granulometría - micrometro',
@@ -18,7 +19,7 @@ def load_model_sens():
     for var in sens_vars:
         models_sens.append(tf.keras.models.load_model(f"models/gcce/gcce_ma_{var}.keras",
                                                       custom_objects={'method': GCCE_MA_loss}))
-    return models.MultiOutputModel(models)
+    return models.MultiOutputModel(models_sens)
 
 model_to_sens = load_model_sens()
 model_to_fq = tf.keras.models.load_model('models/gcce/model_s2fq.keras')
@@ -32,9 +33,9 @@ async def predict_sens(
     viscosidad: float = Query(...),
     fluidez: float = Query(...)
 ):
-    input_data = [[humedad, grasa,granulometria, viscosidad, fluidez]]
+    input_data = np.array([[humedad, grasa,granulometria, viscosidad, fluidez]])
     predictions = model_to_sens.predict(input_data)
-    return {"sens": predictions}
+    return {"sens": predictions.tolist()}
 
 @app.get("/predict_fq")
 async def predict_fq(
@@ -47,6 +48,6 @@ async def predict_fq(
     impresion: float = Query(...),
     fusion: float = Query(...)
 ):
-    input_data = [[acidez, amargor, aroma, astringencia, dulce, dureza, impresion, fusion]]
+    input_data = np.array([[acidez, amargor, aroma, astringencia, dulce, dureza, impresion, fusion]])
     predictions = model_to_fq.predict(input_data)
-    return {"fq": predictions}
+    return {"fq": predictions.tolist()}
