8784c9b90872b94271b7b07e7952435b5b720ced
jclugor <jclugor@unal.edu.co>
2024-09-09 17:03:22 -0500

    retrain fq

--- main.py
+++ main.py
@@ -23,9 +23,19 @@ model_to_sens = load_model_sens()
 model_to_fq = tf.keras.models.load_model('models/gcce/model_s2fq.keras')
 
 # %% db connection
-def get_db_connection():
-    conn = sqlite3.connect('data/chocolate/databases/chocolate_new.db')
+OLD_DATA = "data/chocolate/databases/chocolate.db"
+NEW_DATA = "data/chocolate/databases/chocolate_new.db"
+
+def get_db_connection(db_path):
+    conn = sqlite3.connect(db_path)
     return conn
+
+def read_from_db(db_path, table_name):
+    conn = sqlite3.connect(db_path)
+    query = f"SELECT * FROM {table_name}"
+    df = pd.read_sql_query(query, conn)
+    conn.close()
+    return df
 
 # %% schemas
 class FqInput(BaseModel):
@@ -54,7 +64,7 @@ async def predict_sens(data: FqInput):
                             data.viscosidad, data.fluidez]])
     
     predictions, _ = model_to_sens.predict(input_data)
-    conn = get_db_connection()
+    conn = get_db_connection(NEW_DATA)
     cursor = conn.cursor()
 
     cursor.execute(
@@ -79,7 +89,7 @@ async def predict_fq(data: SensInput):
                             data.dulce, data.dureza, data.impresion, data.fusion]])
     predictions = model_to_fq.predict(input_data)
 
-    conn = get_db_connection()
+    conn = get_db_connection(NEW_DATA)
     cursor = conn.cursor()
 
     cursor.execute(
@@ -97,3 +107,26 @@ async def predict_fq(data: SensInput):
     conn.close()
 
     return {"fq_predictions": dict(zip(FQ_VARS_CHOC, pred_values))}
+
+@app.post("/retrain_model_fq")
+async def retrain_model_fq():
+    old_df_sens = read_from_db(OLD_DATA, 'pred_sens')
+    new_df_sens = read_from_db(NEW_DATA, 'pred_sens')
+    df_sens = pd.concat([old_df_sens, new_df_sens], ignore_index=True).set_index('cod_sampler')
+
+    old_df_fq = read_from_db(OLD_DATA, 'train_fq')
+    new_df_fq = read_from_db(NEW_DATA, 'train_fq')
+    df_fq = pd.concat([old_df_fq, new_df_fq], ignore_index=True).set_index('cod_sampler')
+
+    idx = df_sens.index.intersection(df_fq.index)
+    df_fq = df_fq[idx]
+    df_sens = df_sens[idx]
+
+    model = models.model_s2fq()
+    model.fit(df_sens.values/10, df_fq.values)
+    model.save("")
+    return ""
+
+@app.post("/retrain_model_sens")
+async def retrain_model_sens():
+    pass
