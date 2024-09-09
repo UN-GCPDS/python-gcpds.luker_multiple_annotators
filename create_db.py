0996637a5bcbe5089e96462feb9e4b56b253bd6a
jclugor <jclugor@unal.edu.co>
2024-09-08 22:14:41 -0500

    data and db added

--- create_db.py
+++ create_db.py
@@ -1,12 +1,15 @@
 import pandas as pd
 import sqlite3
+import os
 
-csv_files = []
+folder = 'data/chocolate'
+csv_files = [f for f in os.listdir(folder) if f.endswith('.csv')]
 
 conn = sqlite3.connect('luker.db')
 
 for csv_file in csv_files:
-    df = pd.read_csv(csv_file)
+    df = pd.read_csv(os.path.join(folder, csv_file))
+    df.columns.values[0] = 'cod_sampler'
     # name table after filename without extension
     table_name = csv_file[:-4]
     df.to_sql(table_name, conn, if_exists='replace', index=False)
