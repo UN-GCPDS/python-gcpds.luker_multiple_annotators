38ff3358319e0a170eefb1179f9d378d4c22e094
jclugor <jclugor@unal.edu.co>
2025-03-26 15:05:55 -0500

    include gpflow in install_requires

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ setup(
     name="ma_models",  # Mandatory
     version="0.1.0",  # Recommended
     packages=find_packages(where='src'),  # Recommended
-    install_requires=[],  # Recommended, even if empty
+    install_requires=['gpflow'],  # Recommended, even if empty
     package_dir={'':'src'}
 )
