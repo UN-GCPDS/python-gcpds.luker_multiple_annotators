a639b43fb487d27c7a390c4946300ff5d6f1a3b6
jclugor <jclugor@unal.edu.co>
2025-03-26 10:16:56 -0500

    update setup

--- setup.py
+++ setup.py
@@ -4,5 +4,6 @@ setup(
     name="ma_models",  # Mandatory
     version="0.1.0",  # Recommended
     packages=find_packages(),  # Recommended
-    install_requires=[]  # Recommended, even if empty
+    install_requires=[],  # Recommended, even if empty
+    package_dir={'':'src'}
 )
