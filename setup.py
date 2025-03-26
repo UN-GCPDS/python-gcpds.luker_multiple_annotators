1f24868513cab0d718a0add0f36df2d30ce59000
jclugor <jclugor@unal.edu.co>
2025-03-26 14:57:34 -0500

    fix setup

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="ma_models",  # Mandatory
     version="0.1.0",  # Recommended
-    packages=find_packages(),  # Recommended
+    packages=find_packages(where='src'),  # Recommended
     install_requires=[],  # Recommended, even if empty
     package_dir={'':'src'}
 )
